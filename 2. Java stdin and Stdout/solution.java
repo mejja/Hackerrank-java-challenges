5dbc446d3fd1fb2b35b6709d258b3b221c7c7f42
mejja <majorkiema.mk@gmail.com>
2025-03-12 20:46:21 +0000

    provided solutions for 2nd challenge

--- 2. Java stdin and Stdout/solution.java
+++ 2. Java stdin and Stdout/solution.java
@@ -5,11 +5,11 @@ public class Solution {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int a = scan.nextInt();
-        // Complete this line
-        // Complete this line
+        int b = scan.nextInt(); // Read the second integer
+        int c = scan.nextInt(); // Read the third integer
 
         System.out.println(a);
-        // Complete this line
-        // Complete this line
+        System.out.println(b); // Print the second integer
+        System.out.println(c); // Print the third integer
     }
 }
